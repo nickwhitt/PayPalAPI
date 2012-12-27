7d8700c2661bb9ef669170c910ab1acaa8adb549
Nick Whitt <nick.whitt@gmail.com>
2012-12-27 10:22:32 -0500

    Creating a phpUnit testsuite environment.

---
+++ tests/NVPTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * NVP Unit Test
+ *
+ * @author Nicholas Whitt <nick.whitt@gmail.com>
+ * @copyright Copyright (c) 2012, Nicholas Whitt
+ * @link https://github.com/nickwhitt/PayPalAPI Source
+ * @license http://www.apache.org/licenses/ Apache License Version 2.0
+ */
+
+namespace PayPalAPI\Tests;
+class NVPTest extends \PHPUnit_Framework_TestCase {
+	public function testSkeleton() {
+		$this->markTestIncomplete('Not yet implemented.');
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Test Suite Bootstrap
+ *
+ * @author Nicholas Whitt <nick.whitt@gmail.com>
+ * @copyright Copyright (c) 2012, Nicholas Whitt
+ * @link https://github.com/nickwhitt/PayPalAPI Source
+ * @license http://www.apache.org/licenses/ Apache License Version 2.0
+ */
+
+require_once dirname(__FILE__) . '/../config.sample.php';
+require_once dirname(__FILE__) . '/../NVP.php';
